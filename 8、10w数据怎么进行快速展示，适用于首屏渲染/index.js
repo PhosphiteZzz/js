dff2e7872638deaf84857bef804f9ff6dddcd504
lixiangjian <13157190729@163.com>
2021-12-20 14:34:27 +0800

    代码提交

---
+++ 8、10w数据怎么进行快速展示，适用于首屏渲染/index.js
@@ -0,0 +1,112 @@
+const getList = () => {
+    return new Promise((resolve, reject) => {
+        //步骤一:创建异步对象
+        var ajax = new XMLHttpRequest();
+        //步骤二:设置请求的url参数,参数一是请求的类型,参数二是请求的url,可以带参数
+        ajax.open('get', 'http://127.0.0.1:8000');
+        //步骤三:发送请求
+        ajax.send();
+        //步骤四:注册事件 onreadystatechange 状态改变就会调用
+        ajax.onreadystatechange = function() {
+            if (ajax.readyState == 4 && ajax.status == 200) {
+                //步骤五 如果能够进到这个判断 说明 数据 完美的回来了,并且请求的页面是存在的
+                resolve(JSON.parse(ajax.responseText))
+            }
+        }
+    })
+}
+
+// 获取container对象
+const container = document.getElementById('container')
+
+// setTimeout分页渲染
+const setTimeoutRender = async() => {
+    console.time("列表时间")
+    const list = await getList()
+    console.log(list)
+    const total = list.length
+    const page = 0
+    const limit = 200
+    const totalPage = Math.ceil(total / limit)
+
+    const render = (page) => {
+        if (page > totalPage) return
+        setTimeout(() => {
+            for (let i = page * limit; i < page * limit + limit; i++) {
+                const item = list[i]
+                const div = document.createElement('div')
+                div.className = 'sunshine'
+                div.innerHTML = `<img src="${item.src}" /><span>${item.text}</span>`
+                container.appendChild(div)
+            }
+            render(page + 1)
+        }, 0)
+    }
+    render(page)
+    console.timeEnd('列表时间')
+
+}
+
+
+// requestAnimationFrame渲染
+const requestAnimationRender = async() => {
+    console.time("列表时间")
+    const list = await getList()
+    console.log(list)
+    const total = list.length
+    const page = 0
+    const limit = 200
+    const totalPage = Math.ceil(total / limit)
+
+    const render = (page) => {
+        if (page > totalPage) return
+        requestAnimationFrame(() => {
+            for (let i = page * limit; i < page * limit + limit; i++) {
+                const item = list[i]
+                const div = document.createElement('div')
+                div.className = 'sunshine'
+                div.innerHTML = `<img src="${item.src}" /><span>${item.text}</span>`
+                container.appendChild(div)
+            }
+            render(page + 1)
+        })
+    }
+    render(page)
+    console.timeEnd('列表时间')
+}
+
+// 文档碎片 + requestAnimationFrame渲染
+const fileRequestAnimationRender = async() => {
+    console.time("列表时间")
+    const list = await getList()
+    console.log(list)
+    const total = list.length
+    const page = 0
+    const limit = 200
+    const totalPage = Math.ceil(total / limit)
+
+    const render = (page) => {
+        if (page > totalPage) return
+        requestAnimationFrame(() => {
+            // 创建一个文档碎片
+            const fragment = document.createDocumentFragment()
+            for (let i = page * limit; i < page * limit + limit; i++) {
+                const item = list[i]
+                const div = document.createElement('div')
+                div.className = 'sunshine'
+                div.innerHTML = `<img src="${item.src}" /><span>${item.text}</span>`
+                    // 先塞进文档碎片
+                fragment.appendChild(div)
+            }
+            // 一次性appendChild
+            container.appendChild(fragment)
+            render(page + 1)
+        })
+    }
+    render(page)
+    console.timeEnd('列表时间')
+}
+
+// setTimeoutRender()
+// requestAnimationRender()
+fileRequestAnimationRender()
